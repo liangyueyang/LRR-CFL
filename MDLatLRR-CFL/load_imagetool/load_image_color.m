d83808b24adfb6761798c5dd52b39e216792a7b8
liangyueyang <80051973+liangyueyang@users.noreply.github.com>
2025-03-22 01:24:24 +0800

    Add files via upload

---
+++ MDLatLRR-CFL/load_imagetool/load_image_color.m
@@ -0,0 +1,16 @@
+function [imgs A_YUV] = load_image_color(paths)
+
+imgs = cell(size(paths));
+for i = 1:numel(paths)
+    X = imread(paths{i});
+    if size(X, 3) == 3 % we extract our features from Y channel
+%         X = rgb2ycbcr(X);
+%         X = X(:, :, 1);
+        A_YUV=ConvertRGBtoYUV(X);   
+        imgs{i}=A_YUV(:,:,1); 
+    else
+imgs{i} = X;
+    end
+%     X = im2single(X); % to reduce memory usage
+    
+end
